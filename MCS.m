0c7119764693d90d6bfbf5d80925afa24c514ca0
sean.walton84 <sean.walton84@gmail.com>
2012-03-08 11:48:50 +0000

    Fixed issue 6

--- MCS.m
+++ MCS.m
@@ -77,6 +77,9 @@ NormFact = zeros(1,NoDim);
 MinFact = zeros(1,NoDim);
 for i=1:NoDim
     NormFact(1,i) = vardef(1,i)-vardef(2,i);
+    if eq(NormFact(1,i),0)
+79	        NormFact(1,i) = 0;
+80	end
     MinFact(1,i) = vardef(2,i);
     NestI(:,i) = (NestI(:,i)-MinFact(1,i))./NormFact(1,i);
 end
